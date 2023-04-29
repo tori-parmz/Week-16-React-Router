c8e8d07b138184dcd28bd18c11b1469d340e8443
tori-parmz <119701826+tori-parmz@users.noreply.github.com>
2023-04-29 14:31:16 -0500

    1/4 of the way completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch
+} from 'react-router-dom';
 
-function App() {
+export default function App() {
+  const posts = [
+    {
+      id: 1,
+      title: 'My first post',
+      date: '4-7-2020',
+      content: 'This is my first post!'
+    },
+    {
+      id: 2,
+      title: 'Checking in',
+      date: '4-7-2020',
+      content: 'Yesterday was a good day'
+    },
+    {
+      id:1,
+      title: 'Vacation time!',
+      date:'4-7-2020',
+      content: 'Finally time to head out!'
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+          <Link to="/friends">Friends</Link>
+          </li>
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/posts">
+            <Posts posts={posts} />
+          </Route>
+          <Route path="/friends">
+            <Friends names={['Tom', 'Sally', 'Samantha']} />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Friends() {
+  return <h2>Friends</h2>
+}
+
+function Posts() {
+  return <h2>Posts</h2>
+}
